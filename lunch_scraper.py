1b55745de068ddb23c28ff38541037df881cc139
Søren Howe Gersager <syrelyre@gmail.com>
2017-09-20 09:01:50 +0200

    refactored into functions

--- lunch_scraper.py
+++ lunch_scraper.py
@@ -24,29 +24,37 @@ MENU_URL = "http://www.dg-mad.dk/Frokostordning/Menuoversigt.html"
 
 ROOT_URL = "http://www.dg-mad.dk"
 
-def get_menu_output():
+def get_week_pattern():
     week_pattern = "Uge {}"
+    now = datetime.now()
+    # If it's weekend, get next week.
+    if now.weekday() > 4:
+        week_pattern = week_pattern.format(now.isocalendar()[1] + 1)
+    else:
+        week_pattern = week_pattern.format(now.isocalendar()[1])
+    return week_pattern
+
+def get_pdf_indexes():
+    now = datetime.now()
+    # If it's friday make the end index "velbekomme".
+    if now.weekday() == 4:
+        start_index = WEEKDAY_DICT[now.weekday()]
+        end_index = "velbekomme"
+    # If it's weekend let's take mondays menu.
+    if now.weekday() > 4:
+        start_index = WEEKDAY_DICT[0]
+        end_index = WEEKDAY_DICT[1]
+    else:
+        start_index = WEEKDAY_DICT[now.weekday()]
+        end_index = WEEKDAY_DICT[now.weekday()+1]    
+    return start_index, end_index
+
+def extract_pdf_output():
+    week_pattern = get_week_pattern()    
 
     response = requests.get(MENU_URL)
 
     soup = bs4.BeautifulSoup(response.text, "html.parser")
-
-    NOW = datetime.now()
-
-    # If it's friday make the end index "velbekomme"
-    if NOW.weekday() == 4:
-        week_pattern = week_pattern.format(NOW.isocalendar()[1])
-        START_INDEX = WEEKDAY_DICT[NOW.weekday()]
-        END_INDEX = "velbekomme"
-    # If it's weekend let's take mondays menu
-    if NOW.weekday() > 4:
-        week_pattern = week_pattern.format(NOW.isocalendar()[1]+1)
-        START_INDEX = WEEKDAY_DICT[0]
-        END_INDEX = WEEKDAY_DICT[1]
-    else:
-        week_pattern = week_pattern.format(NOW.isocalendar()[1])
-        START_INDEX = WEEKDAY_DICT[NOW.weekday()]
-        END_INDEX = WEEKDAY_DICT[NOW.weekday()+1]
 
     # Grab current weeks PDF menu.
     elem = soup.find("strong", text=re.compile(week_pattern))
@@ -58,8 +66,13 @@ def get_menu_output():
         response.raw.decode_content = True
         shutil.copyfileobj(response.raw, file)
         output = subprocess.check_output(["pdftotext", file.name, "-"]).lower().decode("utf-8")
+    return output
 
-    RE_STRING = r"({}.*?){}".format(START_INDEX, END_INDEX)
+def get_menu_output():
+    output = extract_pdf_output()
+    start_index, end_index = get_pdf_indexes()
+   
+    RE_STRING = r"({}.*?){}".format(start_index, end_index)
 
     regex_object = re.compile(RE_STRING, re.DOTALL)
     menu_output = re.search(regex_object, output).group(1)
