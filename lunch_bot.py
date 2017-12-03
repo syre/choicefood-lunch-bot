74372afc5547167b7c9fb977d85417c13c2052f9
Søren Howe Gersager <syrelyre@gmail.com>
2017-12-03 15:22:10 +0000

    add echo_response

--- lunch_bot.py
+++ lunch_bot.py
@@ -8,6 +8,7 @@ from tasks import *
 bot = MsBot()
 bot.add_process(fetch_lunch_response)
 bot.add_process(rate_lunch_response)
+bot.add_process(echo_response)
 
 if __name__ == '__main__':
 	bot.run()
