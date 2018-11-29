f792ff9d7a0a25c68812d7c89a5ec46184e1180f
Søren Howe Gersager <syrelyre@gmail.com>
2018-11-29 20:23:54 +0100

    added examples

--- teams_webhook.py
+++ teams_webhook.py
@@ -11,13 +11,8 @@ Eg. email received 6:00, script runs at 7:00 and does not send the payload.
     email received yesterday at 7:00, script runs at 7:00, sends the payload.
 """
 import sys
-from datetime import (
-    datetime,
-    timedelta,
-)
 
 import requests
-from pytz import timezone
 
 from utils import (
     is_in_previous_hour,
@@ -32,7 +27,8 @@ from lunch_scraper import (
 
 WEB_HOOK_POST_URL = (
     "https://outlook.office.com/webhook/"
-    "f359ff3a-7f6f-4962-b62c-a12330b100fa@e62d78b0-89a4-4d47-81c0-03d7b05d12f1/"
+    "f359ff3a-7f6f-4962-b62c-a12330b100fa@"
+    "e62d78b0-89a4-4d47-81c0-03d7b05d12f1/"
     "IncomingWebhook/0e90fff3dc224311ae02e3f730c0a707/"
     "0106ec0d-7bf2-46d9-8192-7408b6d52db3"
 )
@@ -47,6 +43,7 @@ if __name__ == '__main__':
     send_message = (is_in_previous_hour(email_time) or
                     is_seven_o_clock_danish_time())
     output = get_menu_output()
+
     if send_message:
         print("sending message!")
         requests.post(
