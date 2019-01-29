42aaf8f4d6d90233a2356d14f397767d6678b150
Søren Howe Gersager <syrelyre@gmail.com>
2019-01-29 13:20:14 +0100

    mustve been drunk

--- lunchbot/utils.py
+++ lunchbot/utils.py
@@ -10,6 +10,7 @@ from datetime import (
 )
 from pytz import timezone
 
+
 def is_seven_o_clock_danish_time(datetime):
     """Check if datetime is in seven o clock range in Denmark."""
     danish_timezone = timezone("Europe/Copenhagen")
@@ -23,15 +24,18 @@ def is_seven_o_clock_danish_time(datetime):
 def is_in_previous_hour(now_datetime, email_datetime):
     """Check if datetime was in the previous hour compared to now."""
     previous_hour = (now_datetime - timedelta(hours=1))
-    if email_datetime.hour == previous_hour.hour:
+    if (email_datetime.date() == previous_hour.date()) and (
+            email_datetime.hour == previous_hour.hour):
         return True
     return False
+
 
 def add_formatting(output):
     """Add some nice formatting to the message"""
     output = re.sub(r"\n{1,}", "\n\n", output)
     output = re.sub(r" {2}", "&nbsp;", output)
     return output
+
 
 def remove_excessive_newlines(output):
     """Replace sequences of more than one newlines with just one"""
