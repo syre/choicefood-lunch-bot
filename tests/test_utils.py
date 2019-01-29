2f2a7bb5ea5b913429f7335a85fe2a401602cd05
Søren Howe Gersager <syrelyre@gmail.com>
2019-01-29 15:24:30 +0100

    remove unused imports, add/remove whitespace

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,16 +10,15 @@ from pytz import timezone
 from lunchbot.utils import (
     is_seven_o_clock_danish_time,
     is_in_previous_hour,
-    add_formatting,
-    remove_excessive_newlines,
 )
+
 
 class TestUtilities():
 
     def test_is_seven_o_clock_true(self):
         seven_o_clock = datetime(2018, 11, 30, 7, 0, 0, 0, tzinfo=timezone("Europe/Copenhagen"))
         assert is_seven_o_clock_danish_time(seven_o_clock) == True
-    
+
     def test_is_seven_o_clock_false(self):
         seven_o_clock = datetime(2018, 11, 30, 7, 0, 0, 0, tzinfo=timezone("UTC"))
         assert is_seven_o_clock_danish_time(seven_o_clock) == False
